5dae924cc869e89fd145f33bb54df52f00d59ce4
Eric Yuri <ericyuri370@gmail.com>
2026-08-25 22:06:19 -0300

    até daqui a pouco

---
+++ apps/api/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('categories', [CategoryController::class, 'index']);
+Route::post('categories', [CategoryController::class, 'store']);
+Route::get('categories/{id}', [CategoryController::class, 'show']);
+Route::put('categosries/{id}', [CategoryController::class, 'update']);
+Route::delete('categosries/{id}', [CategoryController::class, 'destroy']);
+
+
